72b23c52890c2d3009aade8708fb3c7d37bf3947
Stewart Armbrecht <stewartarmbrecht@gmail.com>
2014-08-25 14:00:31 -0500

    Working on getting heroku up and running.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.set('case sensitive routing', false);
-app.use(app.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.redirect('/gherkinRunner.html');
--- public/features/featureSet.js
+++ public/features/featureSet.js
@@ -2,7 +2,7 @@
   name: "Create New To Do List",  //This is the name that will display in the Gherkin Runner.
   featurePaths: [
     "features/createToDoList", //This is the path to the file without the .html extension.
-    "features/stepParameterTypes"
+    "features/stepArgumentTypes"
   ],
   libraryPaths: [
     "features/library"
