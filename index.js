2765be4ac6ca8dc77afd26518643466bcd791390
Stewart Armbrecht <stewartarmbrecht@gmail.com>
2014-08-25 12:09:08 -0500

    Working on getting heroku up and running.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
+process.env.PWD = process.cwd();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(process.env.PWD + '/public'));
 
 app.listen(process.env.PORT || 3000);
 console.log('listening on port ' + process.env.PORT || 3000);
