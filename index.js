0dee86069c83282f53c94a2d41f482ca9c40714d
Stewart Armbrecht <stewartarmbrecht@gmail.com>
2014-08-25 13:51:25 -0500

    Working on getting heroku up and running.

--- index.js
+++ index.js
@@ -2,16 +2,8 @@ var express = require('express')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-
-// insert this before your routes
-app.use(function(req, res, next) {
-  for (var key in req.query)
-  {
-    req.query[key.toLowerCase()] = req.query[key];
-  }
-  next();
-});
+app.set('case sensitive routing', false);
+app.use(app.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.redirect('/gherkinRunner.html');
